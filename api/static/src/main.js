35a2b0f695cb5772883feffcc10dc41ccbef2044
yumekiti <yumekiti120440@gmail.com>
2021-12-03 14:27:17 +0900

    add: axios

--- api/static/src/main.js
+++ api/static/src/main.js
@@ -3,5 +3,9 @@ console.log(
 );
 
 let app = document.getElementById("app")
-
 app.innerHTML = "<h1>Hello, World</h1>"
+
+.get('/').then((response) => {
+    console.log(response);
+    app.innerHTML += "<p>" + response.data.Hello + "</p>"
+})
